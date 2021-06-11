d18decac1b4ba42b474d462f356456953c1b3496
Tyson Code Cademy <Tyson3rd@Michelles-MacBook-Air.local>
2021-06-11 09:41:43 -0500

    First commit

---
+++ src/main/java/com/example/Restservice/practice/RestServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Restservice.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Restservice/practice/RestServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Restservice.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
